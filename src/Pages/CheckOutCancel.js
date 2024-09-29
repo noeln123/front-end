aff1d2f020860cf7e60296a012596df11f431fcb
dinhtuanviet08 <vietdinh966@gmail.com>
2024-09-29 21:00:18 +0700

    thêm giao diện trang thành công và thất thại khi thanh toán

--- src/Pages/CheckOutCancel.js
+++ src/Pages/CheckOutCancel.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
+import '../Resource/Css/viet-all.css';
 import { HeaderMenu } from "../Component/Menu";
 import Footer from "../Component/Footer";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Link } from 'react-router-dom';
 
 const CheckOutCancel = () => {
     return (
         <>
             <HeaderMenu />
-            <h2>NO SIRRRRRRRRRRRR </h2> 
-            THANH TOAN THAT CMN BAI
+            <div className="checkout-cancel-container">
+                <div className="cancel-message">
+                    <i className="fa-solid fa-triangle-exclamation"></i>
+                    <h1>Payment Failed</h1>
+                    <p>Unfortunately, your payment for the course could not be processed.</p>
+                    <p>Please check your payment details or try again later.</p>
+                    <button className="btn-retry">Retry Payment</button>
+                    <button className="btn btn-backtocourse"><Link to={"/courses"}>Back to Courses</Link></button>
+                </div>
+            </div>
             <Footer />
         </>
     );
